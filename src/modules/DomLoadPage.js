ec329137ef50be4cf2e19697d4deff6121a29a9f
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-11-22 08:54:33 +0100

    Add title and norse font

--- src/modules/DomLoadPage.js
+++ src/modules/DomLoadPage.js
@@ -10,11 +10,16 @@ export default function loadPage() {
   const sidebar = document.createElement("div");
   const toDos = document.createElement("div");
   const footer = document.createElement("div");
+  const title = document.createElement("div");
+
+  title.innerText = "Odinist";
 
   wrapper.classList.add("wrapper");
   sidebar.classList.add("sidebar");
   toDos.classList.add("toDos");
+  title.classList.add("title");
 
+  sidebar.appendChild(title);
   wrapper.appendChild(sidebar);
   wrapper.appendChild(toDos);
   content.appendChild(wrapper);
