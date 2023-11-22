ff2eebf1d238999c08a46d3d4e6698d772644aa4
Stefan Konzelmann <stefan.konzelmann@gmx.de>
2023-11-22 21:42:15 +0100

    Implement adding of task with dialog

--- src/modules/DomLoadPage.js
+++ src/modules/DomLoadPage.js
@@ -1,7 +1,9 @@
 import "../style.css";
 import loadDialog from "./DomLoadDialog";
+import createToDo from "./toDoCreator";
 
 export default function loadPage() {
+  // Build static page
   const content = document.getElementById("content");
   content.innerHTML = "";
 
@@ -12,6 +14,20 @@ export default function loadPage() {
   const footer = document.createElement("div");
   const title = document.createElement("div");
   const btnAddToDo = document.createElement("button");
+
+  const dialog = `
+    <dialog id="taskDialog">
+      <form method="dialog">
+        <p>
+          <label for="taskTitle">Task Title</label><br />
+          <input type="text" id="taskTitle" name="taskTitle" />
+        </p>
+        <div class="formButtons">
+          <button id="cancel" type="reset">Cancel</button>
+          <button id="submit" type="submit">Save</button>
+        </div>
+      </form>
+    </dialog>`;
 
   title.innerText = "Odinist";
   btnAddToDo.innerText = "+ Add Task";
@@ -28,7 +44,26 @@ export default function loadPage() {
   wrapper.appendChild(toDos);
   content.appendChild(wrapper);
 
+  content.insertAdjacentHTML("afterbegin", dialog);
+
+  // Build interaction --> add Task modal
+  const taskDialog = document.querySelector("#taskDialog");
+  const inputTaskTitle = document.querySelector("#taskTitle");
+  const btnSubmit = document.querySelector("#submit");
+  const btnCancel = document.querySelector("#cancel");
+
   btnAddToDo.addEventListener("click", () => {
-    loadDialog();
+    taskDialog.showModal();
+  });
+
+  btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault;
+    createToDo(inputTaskTitle.value);
+    inputTaskTitle.value = "";
+    taskDialog.close();
+  });
+
+  btnCancel.addEventListener("click", () => {
+    taskDialog.close();
   });
 }
--- src/modules/toDoCreator.js
+++ src/modules/toDoCreator.js
@@ -1,8 +1,8 @@
 import ToDo from "./toDoClass";
 import addToDoToPage from "./DomDisplayToDo";
 
-export default function createToDo(toDo) {
+export default function createToDo(title) {
   // toDo object is not yet taken into account
-  const newToDo = new ToDo("Test Task");
+  const newToDo = new ToDo(title);
   addToDoToPage(newToDo);
 }
